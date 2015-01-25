393b5e69c82ee4f0283d6409c7e0903d3f416529
khromov <stanislav.khromov@gmail.com>
2015-01-25 23:01:06 +0100

    Preparing for initial release

--- visual_composer_template_manager.php
+++ visual_composer_template_manager.php
@@ -8,63 +8,62 @@ Author URI: http://snippets.khromov.se
 */
 
 //Prefix for hooks
-$VCTM_PREFIX = 'vctm_';
+$VCTM_PREFIX = 'vctm';
 
 /** Hook into VC and assembles "Default Templates" data **/
 add_action('after_setup_theme', function()
 {
-    add_filter('vc_load_default_templates', function($templates)
-    {
-        global $VCTM_PREFIX;
+    global $VCTM_PREFIX;
 
+    /** Optionally remove the default templates **/
+    add_filter('vc_load_default_templates', function($templates) use ($VCTM_PREFIX)
+    {
+        if(apply_filters("{$VCTM_PREFIX}_disable_builtin_templates", false))
+            return array();
+        else
+            return $templates;
+    }, 11);
+
+    /** Add our templates **/
+    add_filter('vc_load_default_templates', function($templates) use ($VCTM_PREFIX)
+    {
         //Load templates from plugin /vc_templates directory
         $templates = array_merge($templates, vctm_load_templates(__DIR__ . "/vc_templates/*.php", 'vctm'));
 
         //Load templates from theme
-        $templates = array_merge($templates, vctm_load_templates( trailingslashit(get_stylesheet_directory_uri()) . 'vc_templates/*.php', 'theme'));
+        $templates = array_merge($templates, vctm_load_templates( trailingslashit(get_stylesheet_directory()) . 'vc_templates/*.php', 'theme'));
 
         //Load additional templates from plugins or themes
         foreach(apply_filters("{$VCTM_PREFIX}_template_locations", array()) as $additional_location)
             $templates = array_merge($templates, vctm_load_templates( trailingslashit(get_stylesheet_directory_uri()) . 'vc_templates/*.php', 'theme'));
 
         return $templates;
-    }, 11);
+    }, 12);
 });
 
 /**
  * Handles template loading
  *
  * @param string $folder
- * @param bool $absolute_path
+ * @param string $hook_prefix
  * @return array
  */
-function vctm_load_templates($folder = 'default_templates', $absolute_path = false)
+function vctm_load_templates($folder = 'default_templates', $hook_prefix = 'vctm')
 {
     global $VCTM_PREFIX;
+
     $templates = array();
 
-    if($absolute_path === true)
-    {
-        $path = $folder;
-    }
-    else if($folder === '_root')
-    {
-        $folder_prefix = 'default';
-        $path =  "/templates/*.php";
-    }
-    else
-        $path =  "/templates/{$folder}/*.php";
-
     //Load default tempaltes
-    foreach (glob(__DIR__ . $path) as $filename)
+    foreach (glob($folder) as $filename)
     {
         $filename_clean = basename($filename, '.php');
 
         $data = array();
-        $data['name']       = __( apply_filters("{$VCTM_PREFIX}_{$folder}_name_{$filename_clean}", vctm_prettify_name($filename_clean)), apply_filters("{$VCTM_PREFIX}_textdomain", 'vc_template_manager') );
-        $data['weight']     = apply_filters("{$VCTM_PREFIX}_{$folder}_weight_{$filename_clean}", apply_filters("{$VCTM_PREFIX}_default_weight", 0));
-        $data['custom_class'] = apply_filters("{$VCTM_PREFIX}_{$folder}_class_{$filename_clean}", $filename_clean);
-        $data['content']    = apply_filters("{$VCTM_PREFIX}_{$folder}_content_{$filename_clean}", file_get_contents($filename));
+        $data['name']       = __( apply_filters("{$VCTM_PREFIX}_{$hook_prefix}_name_{$filename_clean}", vctm_prettify_name($filename_clean)), apply_filters("{$VCTM_PREFIX}_textdomain", 'vc_template_manager') );
+        $data['weight']     = apply_filters("{$VCTM_PREFIX}_{$hook_prefix}_weight_{$filename_clean}", apply_filters("{$VCTM_PREFIX}_default_weight", 0));
+        $data['custom_class'] = apply_filters("{$VCTM_PREFIX}_{$hook_prefix}_class_{$filename_clean}", $filename_clean);
+        $data['content']    = apply_filters("{$VCTM_PREFIX}_{$hook_prefix}_content_{$filename_clean}", file_get_contents($filename));
 
         $templates[] = $data;
     }
